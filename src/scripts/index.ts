aa9083cd82e14c40a0c91efd58e64d4a751ee297
Krassnig <niklas@krassnig.dev>
2020-09-05 17:48:20 +0200

    Add import to index script
    add encoding in index.html

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,3 @@
+import { Program } from "./program";
+
 Program.main(window.location.search.substring(1).split(/(\&|=)/));
--- src/scripts/program.ts
+++ src/scripts/program.ts
@@ -1,4 +1,4 @@
-class Program {
+export class Program {
 	public static main(args: string[]): void {
 		console.log('Hello World!');
 	}
