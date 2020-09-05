ea4c961e2221e9072acec85a278fba50dd59e71b
Krassnig <niklas@krassnig.dev>
2020-09-05 18:52:31 +0200

    Add mathematical oprations to matrix

--- src/scripts/matrix.ts
+++ src/scripts/matrix.ts
@@ -15,6 +15,63 @@ class Matrix {
 		return this.m[row][col];
 	}
 
+	public normalize(): Matrix {
+		const result = Matrix.createNull(this.rows + 1, this.cols + 1);
+
+		for (let r = 0; r < result.rows; r++) {
+			for (let c = 0; c < result.cols; c++) {
+				result.m[r][c] = this.m[r][c];
+			}
+		}
+
+		result.m[result.rows - 1][result.cols - 1] = 1;
+		return result;
+	}
+
+	public static add(m1: Matrix, m2: Matrix): Matrix {
+		if (m1.rows !== m2.rows || m1.cols !== m2.cols) throw new Error('Matrices are not the same size.');
+
+		const result = Matrix.createNull(m1.rows, m1.cols);
+
+		for (let r = 0; r < result.rows; r++) {
+			for (let c = 0; c < result.cols; c++) {
+				result.m[r][c] = m1.m[r][c] + m2.m[r][c];
+			}
+		}
+
+		return result;
+	}
+
+	public static sub(m1: Matrix, m2: Matrix): Matrix {
+		if (m1.rows !== m2.rows || m1.cols !== m2.cols) throw new Error('Matrices are not the same size.');
+
+		const result = Matrix.createNull(m1.rows, m1.cols);
+
+		for (let r = 0; r < result.rows; r++) {
+			for (let c = 0; c < result.cols; c++) {
+				result.m[r][c] = m1.m[r][c] - m2.m[r][c];
+			}
+		}
+
+		return result;
+	}
+
+	public static mult(a: Matrix, b: Matrix): Matrix {
+		if (a.cols !== b.rows) throw new Error('');
+
+		const result = Matrix.createNull(a.rows, b.cols);
+
+		for (let ar = 0; ar < a.rows; ar++) {
+			for (let bc = 0; bc < b.cols; bc++) {
+				for (let i = 0; i < a.cols; i++) {
+					result.m[ar][bc] += a.m[ar][i] * b.m[i][bc];
+				}
+			}
+		}
+
+		return result;
+	}
+
 	public static create(matrix: number[][]): Matrix {
 		const m = matrix;
 		const r = matrix.length;
