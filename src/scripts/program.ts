17c922c88adb332f67ffe108ecb2c5ecfdfd52df
Krassnig <niklas@krassnig.dev>
2020-09-06 00:21:25 +0200

    Implement rectangle and make test case

--- src/scripts/program.ts
+++ src/scripts/program.ts
@@ -1,21 +1,20 @@
 import Canvas from "./canvas";
 import {Polygon} from "./shapes/polygon";
 import {Animator} from "./animator";
+import { Rectangle } from "./shapes/rectangle";
 
 export class Program {
 	public static async main(args: string[]): Promise<void> {
 		const c = await Canvas.create();
 
-		const polygon = new Polygon([[100,100],[100,300],[600,300],[600,100]]);
+		const polygon = new Rectangle(100, 100, 100, 100);
 		const animator =
 			new Animator(c, polygon)
-				.translate(0, 100)
-				.animate(5)
-				.finalize();
-
-		const animator2 =
-			new Animator(c, polygon)
-				.animate(5)
+				.translate(-150, -150)
+				.rotate(Math.PI / 2)
+				.translate(150, 150)
+				.translate(0, 200)
+				.animate(2)
 				.finalize();
 	}
 }
--- src/scripts/shapes/rectangle.ts
+++ src/scripts/shapes/rectangle.ts
@@ -1,17 +1,42 @@
 import Canvas, {Drawable} from "../canvas";
+import Shape from "./shape";
+import Matrix from "../matrix";
+import { Config } from "../config";
 
-export class Rectangle implements Drawable{
-    draw(canvas: Canvas): void {
-        //canvas.drawRect();
-    }
+export class Rectangle implements Shape {
     constructor(x: number, y: number, w: number, h: number) {
         this.x = x;
         this.y = y;
         this.width = w;
         this.height = h;
-    }
+	}
+	
     private x: number;
     private y: number;
     private width: number;
-    private height: number;
+	private height: number;
+	
+	draw(canvas: Canvas, vectorList: Matrix): void {
+		const c = canvas.c;
+		c.fillStyle = Config.color;
+		
+        c.beginPath();
+		c.moveTo(vectorList.get(0, 0), vectorList.get(1, 0));
+		
+        for (let i = 0; i < vectorList.cols; i++) {
+            c.lineTo(vectorList.get(0, i), vectorList.get(1, i));
+		}
+		
+        c.closePath();
+        c.stroke();
+	}
+
+	toVectorList(): Matrix {
+		return Matrix.create([
+			[this.x, this.y],
+			[this.x + this.width, this.y],
+			[this.x + this.width, this.y + this.height],
+			[this.x, this.y + this.height ]
+		]).transpose().normalizeAsVectorList();
+	}
 }
