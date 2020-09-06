ce0b1c8609cd174733e2c2ef1c343973a36c6d9d
Nikolaus Kasyan <niki.kasyan@gmail.com>
2020-09-06 19:19:51 +0200

    Update draw method with color and drawtype

--- src/scripts/shapes/rectangle.ts
+++ src/scripts/shapes/rectangle.ts
@@ -1,7 +1,8 @@
-import Canvas, {Drawable} from "../canvas";
-import Shape from "./shape";
+import Canvas from "../canvas";
+import Shape, {DrawType} from "./shape";
 import Matrix from "../matrix";
-import { Config } from "../config";
+import {Config} from "../config";
+import {Vector} from "../vector";
 
 export class Rectangle implements Shape {
     constructor(x: number, y: number, w: number, h: number) {
@@ -9,48 +10,37 @@ export class Rectangle implements Shape {
         this.y = y;
         this.width = w;
         this.height = h;
-	}
-	
+    }
+
     private x: number;
     private y: number;
     private width: number;
-	private height: number;
+    private height: number;
 
-	preview(canvas: Canvas, vectorList: Matrix): void{
-		const c = canvas.c;
-		c.fillStyle = Config.color;
+    draw(canvas: Canvas, vectorList: Matrix, color: string = Config.color, drawType: DrawType = DrawType.FILL): void {
+        const ctx = canvas.c;
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+        console.log(color);
+        ctx.beginPath();
+        ctx.moveTo(vectorList.get(0, 0), vectorList.get(1, 0));
 
-		c.beginPath();
-		c.moveTo(vectorList.get(0, 0), vectorList.get(1, 0));
-
-		for (let i = 0; i < vectorList.cols; i++) {
-			c.lineTo(vectorList.get(0, i), vectorList.get(1, i));
-		}
-
-		c.closePath();
-		c.stroke();
-	}
-	draw(canvas: Canvas, vectorList: Matrix): void {
-		const c = canvas.c;
-		c.fillStyle = Config.color;
-		
-        c.beginPath();
-		c.moveTo(vectorList.get(0, 0), vectorList.get(1, 0));
-		
         for (let i = 0; i < vectorList.cols; i++) {
-            c.lineTo(vectorList.get(0, i), vectorList.get(1, i));
-		}
-		
-        c.closePath();
-        c.fill();
-	}
+            ctx.lineTo(vectorList.get(0, i), vectorList.get(1, i));
+        }
 
-	toVectorList(): Matrix {
-		return Matrix.create([
-			[this.x, this.y],
-			[this.x + this.width, this.y],
-			[this.x + this.width, this.y + this.height],
-			[this.x, this.y + this.height ]
-		]).transpose().normalizeAsVectorList();
-	}
+        ctx.closePath();
+        if (drawType === DrawType.FILL) ctx.fill();
+        else if (drawType === DrawType.STROKE) ctx.stroke();
+        else throw new Error("Unknown DrawType " + drawType);
+    }
+
+    toVectorList(): Matrix {
+        return Matrix.create([
+            [this.x, this.y],
+            [this.x + this.width, this.y],
+            [this.x + this.width, this.y + this.height],
+            [this.x, this.y + this.height]
+        ]).transpose().normalizeAsVectorList();
+    }
 }
--- src/scripts/shapes/shape.ts
+++ src/scripts/shapes/shape.ts
@@ -1,8 +1,14 @@
 import Canvas from "../canvas";
 import Matrix from "../matrix";
+import {Config} from "../config";
 
-export default interface Shape {
-	draw(canvas: Canvas, vectorList: Matrix): void;
-	toVectorList(): Matrix;
-	preview(canvas: Canvas, mnext: Matrix): void;
+export default abstract class Shape {
+    draw(canvas: Canvas, vectorList: Matrix, color: string = Config.color, drawType: DrawType = DrawType.STROKE): void {
+    }
+
+    abstract toVectorList(): Matrix;
+}
+
+export enum DrawType {
+    STROKE, FILL
 }
