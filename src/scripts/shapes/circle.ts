adaa7eb76febf9a02292bc823e2f8d7d17d99165
Krassnig <niklas@krassnig.dev>
2020-09-15 13:27:01 +0200

    Circle Example

--- src/scripts/shapes/circle.ts
+++ src/scripts/shapes/circle.ts
@@ -1,13 +1,24 @@
 import Shape from "./shape";
 import Canvas from "../canvas";
 import Matrix from "../matrix";
+import { Vector, Vector2D } from "../vector";
 
 class Circle implements Shape {
+	public constructor(center: [number, number], radius: number) {
+		this.vectors = [
+			Vector2D.createVector2D(...center),
+			Vector2D.createVector2D(center[0], center[0] + radius)
+		];
+	}
+
+	private vectors: Vector2D[];
+
 	draw(canvas: Canvas, vectorList: Matrix): void {
-		throw new Error("Method not implemented.");
+		
+		
 	}
 	toVectorList(): Matrix {
-		throw new Error("Method not implemented.");
+		throw new Error('Not Implemented');
 	}
 	preview(canvas: Canvas, mnext: Matrix): void {
 		throw new Error("Method not implemented.");
--- src/scripts/shapes/shape.ts
+++ src/scripts/shapes/shape.ts
@@ -3,8 +3,7 @@ import Matrix from "../matrix";
 import {Config} from "../config";
 
 export default abstract class Shape {
-    draw(canvas: Canvas, vectorList: Matrix, color: string = Config.color, drawType: DrawType = DrawType.STROKE): void {
-    }
+    draw(canvas: Canvas, vectorList: Matrix, color: string = Config.color, drawType: DrawType = DrawType.STROKE): void { }
 
     abstract toVectorList(): Matrix;
 }
